4c62df7b198e0183c737bfad8a25a7fd7fa9e24b
action jackson <action@localhost.localdomain>
2016-09-09 13:02:03 -0600

    added gitignore, npmignore added dependencies to package.json added tsconfig added pipes barrel.

--- src/pipes/pipes.ts
+++ src/pipes/pipes.ts
@@ -1,7 +1,6 @@
 export * from './propercase.pipe';
 export * from './title.pipe';
 export * from './emphasis.pipe';
-// export * from './policynet.pipe';
 export * from './string-replace.pipe';
 export * from './url.pipe';
 
@@ -9,10 +8,9 @@ import {ProperCasePipe} from './propercase.pipe';
 import {TitleCasePipe} from './title.pipe';
 import {EmphasisPipe} from './emphasis.pipe';
 import {StringReplaceTemplatePipe} from './string-replace.pipe';
-// import {PolicynetPipe} from './policynet.pipe';
 import {UrlPipe} from './url.pipe';
 
 
 // some shared functionality
-export const NG_POLICYNET_PIPES = [ProperCasePipe, TitleCasePipe, EmphasisPipe, /*PolicynetPipe,*/ StringReplaceTemplatePipe, UrlPipe ];
+export const SRP_PIPES = [ProperCasePipe, TitleCasePipe, EmphasisPipe, StringReplaceTemplatePipe, UrlPipe ];
 
---
+++ src/string-replace-pipes.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SRP_PIPES } from './pipes/pipes';
+@NgModule({
+    imports: [],
+    exports: [SRP_PIPES],
+    declarations: [SRP_PIPES],
+   providers: [],
+})
+export class StringReplacePipesModule { }
