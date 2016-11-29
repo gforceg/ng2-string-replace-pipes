57c579de5ac3e5672f067816d944d9cc28abf1d8
action jackson <action@localhost.localdomain>
2016-11-29 13:15:11 -0700

    re-order barrel

--- src/pipes/index.ts
+++ src/pipes/index.ts
@@ -1,6 +1,6 @@
+export { StringReplaceTemplatePipe } from './string-replace.pipe';
 export { EmphasisPipe } from './emphasis.pipe';
 export { ProperCasePipe } from './propercase.pipe';
-export { StringReplaceTemplatePipe } from './string-replace.pipe';
 export { TitleCasePipe } from './title.pipe';
 export { UrlPipe } from './url.pipe';
 
