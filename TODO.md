ae8538cf9b43ca317c47cfd163c23823cc2cc660
Greg Hedin <greghedin@gmail.com>
2017-05-23 15:32:38 -0700

    rebuild

---
+++ TODO.md
@@ -0,0 +1,7 @@
+### FIXMEs
+| Filename | line # | FIXME
+|:------|:------:|:------
+| pipes/propercase.pipe.ts | 3 | allow the user to pipe in a series of domain specific acronyms.
+| pipes/propercase.pipe.ts | 11 | this removes duplicate strings... which should probably be its own pipe!
+| pipes/title.pipe.ts | 14 | this removes duplicate strings... which should probably be its own pipe!
+| set-build-vars.js | 11 | separate this into 'set ignores' and append to .npmignore and .gitignore
---
+++ index.ts
@@ -0,0 +1,7 @@
+export { StringReplaceTemplatePipe } from './ngx-string-replace-pipes/pipes/string-replace.pipe';
+export { EmphasisPipe } from './ngx-string-replace-pipes/pipes/emphasis.pipe';
+export { ProperCasePipe } from './ngx-string-replace-pipes/pipes/propercase.pipe';
+export { TitleCasePipe } from './ngx-string-replace-pipes/pipes/title.pipe';
+export { UrlPipe } from './ngx-string-replace-pipes/pipes/url.pipe';
+export { StringReplacePipesModule } from './ngx-string-replace-pipes/string-replace-pipes.module';
+
